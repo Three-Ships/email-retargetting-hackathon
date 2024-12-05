496c7a18bef3d80568c421fd83f1d6fc5507fe30
Joshua Barker <josh.barker@homesolutions.com>
2024-12-05 11:56:49 -0500

    landing page changes

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // set the first_name to the span with class name
   document.querySelector("span.name").textContent = first_name;
+
+  // gotosuccess function
+  document.provider_listing.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // redirect to success.html
+    window.location.href = `success.html?first_name=${first_name}`;
+  });
 });
